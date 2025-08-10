ac6f448d2985c4112da43884e7bbcaf838cca5c1
ha-quang.le <winlp4ever@gmail.com>
2025-08-10 23:14:52 +0000

    feat(editor): use milkdown for better markdown support instead

---
+++ webui/src/components/editor/milkdown.tsx
@@ -0,0 +1,37 @@
+import { Crepe } from "@milkdown/crepe"
+import { Milkdown, useEditor } from "@milkdown/react"
+
+import "@milkdown/crepe/theme/common/style.css"
+import "@milkdown/crepe/theme/frame.css"
+import "./milkdown-theme.css"
+
+
+/**
+ * Props for the Markdown editor component.
+ */
+export interface MdEditorProps {
+  markdown: string;
+  onSave: (markdown: string) => void
+}
+
+
+/**
+ * Markdown editor component.
+ */
+export const MdEditor = ({ markdown, onSave }: MdEditorProps) => {
+  useEditor((root) => {
+    const crepe = new Crepe({
+      root,
+      defaultValue: markdown,
+    })
+    crepe.on((listener) => {
+      listener.markdownUpdated((_, markdown) => {
+        onSave(markdown)
+      })
+    })
+
+    return crepe
+  }, [])
+
+  return <Milkdown />
+}

756d46498d2dbe58b16928faa2b034b7be562078
Ha Quang Le <winlp4ever@gmail.com>
2025-08-13 02:23:35 +0200

    style(ui/): fix node resolution when zoom-in (#40)

--- webui/src/features/board/types/style.ts
+++ webui/src/features/board/types/style.ts
@@ -54,7 +54,7 @@ export const defaultStyle = (): Style => ({
   strokeStyle: "solid",
   backgroundColor: "oklch(0.954 0.038 75.164)",
   fillStyle: "solid",
-  roughness: 0.2,
+  roughness: 0.1,
   opacity: 100,
   groupIds: [],
   fontFamily: undefined,

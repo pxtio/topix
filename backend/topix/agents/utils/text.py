fd08e97999d2e9b377c3b3cb7df99e90febbad6e
wenzhuo1112 <wenzhuo.liu@stellia.ai>
2025-10-22 14:04:20 +0000

    feat(assistant): postprocess wrongly cited url

--- backend/topix/agents/utils/text.py
+++ backend/topix/agents/utils/text.py
@@ -35,3 +35,83 @@ def extract_final_answer(text: str) -> str:
 
     # Strip leading/trailing whitespace
     return result.strip()
+
+
+def post_process_url_ciation(answer: str, valid_urls: list[str]) -> str:
+    """Post process url citation to correct wrong urls."""
+    valid_url_set = set(valid_urls)
+
+    trie = _create_trie(valid_url_set)
+
+    def replacer(match: re.Match) -> str:
+        extracted_url = match.group(0)
+
+        # check exact match
+        if extracted_url in valid_url_set:
+            return extracted_url
+
+        # correct the url
+        corrected_url = _get_valid_url(extracted_url, trie)
+
+        if corrected_url:
+            return corrected_url
+        else:
+            return extracted_url
+
+    http_link_regex = r'(https?://[^\s()<>\[\]]*)'
+
+    corrected_answer = re.sub(http_link_regex, replacer, answer)
+    return corrected_answer
+
+
+def _create_trie(urls: set[str]) -> dict:
+    """Build a dictionary-based Trie from a list of URLs.
+
+    Args:
+        urls: A list of unique URL strings.
+
+    Returns:
+        A dictionary representing the Trie structure.
+
+    """
+    root = {}
+
+    for url in urls:
+        node = root
+        for char in url:
+            node: dict = node.setdefault(char, {})
+
+        node['#END#'] = True
+
+    return root
+
+
+def _get_valid_url(url: str, trie: dict) -> str:
+    """Find the most similar valid_url from trie.
+
+    Args:
+        url: The URL string to check.
+        trie: The Trie dictionary.
+
+    Returns:
+        The most similar.
+
+    """
+    node = trie
+    valid_url = ""
+
+    for char in url:
+        if char in node:
+            valid_url += char
+            node = node[char]
+
+            # If this node marks the end of a valid URL, update last_valid_url
+            if '#END#' in node:
+                return valid_url
+
+    while "#END#" not in node:
+        key = next(iter(node.keys()))
+        valid_url += key
+        node = node[key]
+
+    return valid_url

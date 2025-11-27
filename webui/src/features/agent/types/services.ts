553dde9c13786da68c51e3f243f9b2164a1ba617
ha quang le <winlp4ever@gmail.com>
2025-11-27 20:16:59 +0000

    refactor(*): minor refactor

--- webui/src/features/agent/types/services.ts
+++ webui/src/features/agent/types/services.ts
@@ -57,10 +57,10 @@ export const defaultServices: () => Services = () => ({
   ],
   search: [
     { name: "linkup", available: false, provider: "linkup" },
+    { name: "exa", available: false, provider: "exa" },
     { name: "perplexity", available: false, provider: "perplexity" },
     { name: "tavily", available: false, provider: "tavily" },
     { name: "openai", available: false, provider: "openai" },
-    { name: "exa", available: false, provider: "exa" },
   ],
   navigate: [
     { name: "tavily", available: false, provider: "tavily" },

e78be3201d5aebb189af559d9176587e7a82cab0
ha quang le <winlp4ever@gmail.com>
2026-02-04 13:59:48 +0000

    chore: reduce thumbnail size

--- webui/src/features/board/hooks/use-make-thumbnail.tsx
+++ webui/src/features/board/hooks/use-make-thumbnail.tsx
@@ -1,42 +1,22 @@
-import { useEffect } from "react"
 import * as htmlToImage from "html-to-image"
 import { saveThumbnail } from "../api/save-thumbnail"
 
+export async function saveThumbnailFromViewport(boardId: string) {
+  if (!boardId) return
+  const el = document.querySelector(".react-flow__viewport") as HTMLElement | null
+  if (!el) return
 
-export function useSaveThumbnailOnUnmount(boardId: string) {
-  useEffect(() => {
-    if (!boardId) return
+  try {
+    const dataUrl = await htmlToImage.toPng(el, {
+      cacheBust: true,
+      pixelRatio: 0.1,
+    })
 
-    let hasRun = false
+    const res = await fetch(dataUrl)
+    const blob = await res.blob()
 
-    const generateAndSave = async () => {
-      if (hasRun) return
-      hasRun = true
-      const el = document.querySelector(
-        ".react-flow__renderer"
-      ) as HTMLElement | null
-
-      if (!el) return
-
-      try {
-        // current view only, no fitView, cheap capture
-        const dataUrl = await htmlToImage.toPng(el, {
-          cacheBust: true,
-          pixelRatio: 1, // fine for thumbnails
-        })
-
-        const res = await fetch(dataUrl)
-        const blob = await res.blob()
-
-        await saveThumbnail({ boardId, blob })
-      } catch (err) {
-        console.error("[useSaveThumbnailOnUnmount] failed", err)
-      }
-    }
-
-    return () => {
-      // fire once on unmount
-      void generateAndSave()
-    }
-  }, [boardId])
+    await saveThumbnail({ boardId, blob })
+  } catch (err) {
+    console.error("[saveThumbnailFromViewport] failed", err)
+  }
 }
